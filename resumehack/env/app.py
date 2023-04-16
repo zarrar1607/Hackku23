358439bd2f266ccf9d57ab930ef35903362d92ea
aaokweb <aaokweb@gmail.com>
2023-04-16 01:39:50 -0500

    final backend push

--- resumehack/env/app.py
+++ resumehack/env/app.py
@@ -10,7 +10,7 @@ import textract
 openai.organization = "org-gG6K1j8fah4HfsIk7JZEGNBO"
 #openai.api_key = "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"
 #openai.api_key = "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"
-openai.api_key = "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"
+openai.api_key = "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"
 app = Flask(__name__)
 
 
@@ -39,6 +39,7 @@ def out():
         # text = textract.process(, method='pdfminer')
         text = page.extract_text()
         text = "".join(text.split(" "))
+    # print(text)
 
     def resume_ai(user_string, text):
         response = openai.ChatCompletion.create(
@@ -49,14 +50,35 @@ def out():
                  }
             ],)
         resume = response['choices'][0]['message']['content']
-    #     # soup = BeautifulSoup(resume)
-    #     # clean_resume = soup.get_text()
-        print(response)
+        # soup = BeautifulSoup(resume)
+        # clean_resume = soup.get_text()
+        # print(response)
         return resume
 
-    clean_text = resume_ai(user_string, text)
+    def coverletter_ai(user_string, text):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": "write a cover letter for my resume {}, make sure you tailor it according to the job description: {} ".format(text, user_string)
+                 }
+            ],)
+        coverletter = response['choices'][0]['message']['content']
+        # soup = BeautifulSoup(resume)
+        # clean_resume = soup.get_text()
+        return coverletter
+
+    resume_text = resume_ai(user_string, text)
+    cover_text = coverletter_ai(user_string, text)
+
+    cl_document = docx.Document()
     r_document = docx.Document()
-    r_document.add_paragraph(clean_text)
+
+    cl_document.add_paragraph(cover_text)
+    r_document.add_paragraph(resume_text)
+
+    cl_document.save('coverletter.docx')
     r_document.save('resume.docx')
+
     # send_file("resume.docx", mimetype=None, as_attachment=True, download_name="download")
-    return send_file("resume.docx", mimetype=None, as_attachment=True, download_name="resume")
+    return send_file("coverletter.docx", mimetype=None, as_attachment=True, download_name="cover")
